f75d942708bcbc7749dfd977982a042b84a3c04a
talhaseeb <talhahaseeb2@gmail.com>
2022-07-03 15:39:07 +0530

    basic authorization 2

--- routes/campground.js
+++ routes/campground.js
@@ -23,7 +23,14 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res) => 
 }));
 
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
+    const campground = await Campground.findById(req.params.id)
+        .populate({
+            path: 'reviews',
+            populate: {
+                path: 'author'
+            }
+        })
+        .populate('author');
     if (!campground) {
         req.flash('error', 'Cannot find the campground');
         return res.redirect('/campgrounds');
