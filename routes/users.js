7c3aa72caffe981243a1f67875a94dfd5e5d3ba1
talhaseeb <talhahaseeb2@gmail.com>
2022-07-03 11:27:11 +0530

    basic authorization part1

--- routes/users.js
+++ routes/users.js
@@ -29,7 +29,7 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 });
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), (req, res) => {
     req.flash('success', 'welcome back!');
     const redirectUrl = req.session.returnTo || '/campgrounds';
     delete req.session.returnTo;
